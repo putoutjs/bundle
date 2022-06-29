1ccc74abab8804b31fff35a6f806d211834d662f
coderaiser <coderaiser@cloudcmd.io>
2022-06-29 19:45:44 +0300

    chore: package: v1.0.0

--- example.js
+++ example.js
@@ -1,11 +1,10 @@
 import putout from 'https://esm.sh/@putout/bundle';
-import removeDebugger from 'https://esm.sh/@putout/plugin-remove-debugger';
+import removeDebugger from 'https://esm.sh/@putout/plugin-remove-debugger?alias=putout:putout/bundle';
+import declare from 'https://esm.sh/@putout/plugin-declare-undefined-variables?alias=putout:putout/bundle';
 
-import removeUnusedVariables from 'https://esm.sh/@putout/plugin-remove-unused-variables?alias=putout:putout/bundle';
-
-console.log(putout('const a = 5;debugger', {
+console.log(putout('const [error] = tryCatch(fn, "hello"); debugger', {
     plugins: [
         ['remove-debugger', removeDebugger],
-        ['remove-unused-variables', remove-unused-variables],
+        ['declare-undefined-variables', declare],
     ]
 }));
