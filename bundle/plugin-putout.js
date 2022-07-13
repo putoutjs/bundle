c7818a5fc371077ee4da8c8cd763e65179caa395
coderaiser <coderaiser@cloudcmd.io>
2022-07-13 21:41:11 +0300

    chore: package: v1.1.3

--- bundle/plugin-putout.js
+++ bundle/plugin-putout.js
@@ -3093,7 +3093,6 @@ function getDynamicModules() {
 		"/node_modules/@putout/plugin-putout/lib/declare/index.js": requireDeclare,
 		"/node_modules/@putout/plugin-putout/lib/includer": requireIncluder,
 		"/node_modules/@putout/plugin-putout/lib/includer/index.js": requireIncluder,
-		"/node_modules/@putout/plugin-putout/lib/index.js": requireLib,
 		"/node_modules/@putout/plugin-putout/lib/move-require-on-top-level": requireMoveRequireOnTopLevel,
 		"/node_modules/@putout/plugin-putout/lib/move-require-on-top-level/index.js": requireMoveRequireOnTopLevel,
 		"/node_modules/@putout/plugin-putout/lib/rename-operate-to-operator": requireRenameOperateToOperator,
@@ -3191,54 +3190,44 @@ function normalize (path) {
 	return path;
 }
 
-var lib = {};
+const getRule = (a) => ({
+    [a]: createCommonjsRequire("/node_modules/@putout/plugin-putout/lib")(`./${a}`),
+});
 
-var hasRequiredLib;
+var rules = {
+    ...getRule('apply-processors-destructuring'),
+    ...getRule('apply-async-formatter'),
+    ...getRule('apply-create-test'),
+    ...getRule('apply-remove'),
+    ...getRule('convert-putout-test-to-create-test'),
+    ...getRule('convert-to-no-transform-code'),
+    ...getRule('convert-find-to-traverse'),
+    ...getRule('convert-destructuring-to-identifier'),
+    ...getRule('convert-number-to-numeric'),
+    ...getRule('convert-replace-with'),
+    ...getRule('convert-replace-with-multiple'),
+    ...getRule('convert-replace-to-function'),
+    ...getRule('convert-match-to-function'),
+    ...getRule('convert-babel-types'),
+    ...getRule('convert-node-to-path-in-get-template-values'),
+    ...getRule('convert-traverse-to-include'),
+    ...getRule('convert-traverse-to-replace'),
+    ...getRule('convert-process-to-find'),
+    ...getRule('convert-method-to-property'),
+    ...getRule('convert-add-argument-to-add-args'),
+    ...getRule('convert-dirname-to-url'),
+    ...getRule('convert-url-to-dirname'),
+    ...getRule('convert-report-to-function'),
+    ...getRule('replace-test-message'),
+    ...getRule('rename-operate-to-operator'),
+    ...getRule('replace-operate-with-operator'),
+    ...getRule('shorten-imports'),
+    ...getRule('check-replace-code'),
+    ...getRule('declare'),
+    ...getRule('add-args'),
+    ...getRule('add-push'),
+    ...getRule('move-require-on-top-level'),
+    ...getRule('includer'),
+};
 
-function requireLib () {
-	if (hasRequiredLib) return lib;
-	hasRequiredLib = 1;
-
-	const getRule = (a) => ({
-	    [a]: createCommonjsRequire("/node_modules/@putout/plugin-putout/lib")(`./${a}`),
-	});
-
-	lib.rules = {
-	    ...getRule('apply-processors-destructuring'),
-	    ...getRule('apply-async-formatter'),
-	    ...getRule('apply-create-test'),
-	    ...getRule('apply-remove'),
-	    ...getRule('convert-putout-test-to-create-test'),
-	    ...getRule('convert-to-no-transform-code'),
-	    ...getRule('convert-find-to-traverse'),
-	    ...getRule('convert-destructuring-to-identifier'),
-	    ...getRule('convert-number-to-numeric'),
-	    ...getRule('convert-replace-with'),
-	    ...getRule('convert-replace-with-multiple'),
-	    ...getRule('convert-replace-to-function'),
-	    ...getRule('convert-match-to-function'),
-	    ...getRule('convert-babel-types'),
-	    ...getRule('convert-node-to-path-in-get-template-values'),
-	    ...getRule('convert-traverse-to-include'),
-	    ...getRule('convert-traverse-to-replace'),
-	    ...getRule('convert-process-to-find'),
-	    ...getRule('convert-method-to-property'),
-	    ...getRule('convert-add-argument-to-add-args'),
-	    ...getRule('convert-dirname-to-url'),
-	    ...getRule('convert-url-to-dirname'),
-	    ...getRule('convert-report-to-function'),
-	    ...getRule('replace-test-message'),
-	    ...getRule('rename-operate-to-operator'),
-	    ...getRule('replace-operate-with-operator'),
-	    ...getRule('shorten-imports'),
-	    ...getRule('check-replace-code'),
-	    ...getRule('declare'),
-	    ...getRule('add-args'),
-	    ...getRule('add-push'),
-	    ...getRule('move-require-on-top-level'),
-	    ...getRule('includer'),
-	};
-	return lib;
-}
-
-requireLib();
+export { rules };
