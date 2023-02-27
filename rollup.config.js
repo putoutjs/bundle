6460aea6f03c852a22551a2618ad54a5cefd0568
coderaiser <coderaiser@cloudcmd.io>
2023-02-27 12:12:39 +0200

    feature: package: @rollup/plugin-commonjs v24.0.1

---
+++ rollup.config.js
@@ -0,0 +1,66 @@
+import commonjs from '@rollup/plugin-commonjs';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+import replace from '@rollup/plugin-replace';
+import alias from '@rollup/plugin-alias';
+import terser from '@rollup/plugin-terser';
+
+const {MINIFY} = process.env;
+
+export default {
+    input: 'lib/putout.js',
+    output: {
+        file: 'bundle/putout.js',
+        format: 'es',
+    },
+    plugins: [
+        MINIFY && terser(),
+        alias({
+            entries: [
+                {find: './loader.mjs', replacement: './lib/loader.js'},
+                {find: 'chalk', replacement: './lib/chalk.js'},
+            ]
+        }),
+        commonjs({
+            defaultIsModuleExports: false,
+            exclude: [
+                '**/lib/loader.*',
+                '**/parse-options/**',
+                'acorn',
+                'tenko*',
+                'hermes-parser/*',
+                'acorn-stage-3',
+                'caniuse',
+                'esprima',
+                'hermes*',
+                'espree',
+                'electron*',
+                'node-releases',
+                'os',
+                'path',
+                'fs',
+                'module',
+                'buffer',
+            ],
+        }),
+        nodeResolve({
+            preferBuiltins: false,
+            browser: true,
+        }),
+        nodePolyfills(),
+        json(),
+        replace({
+            preventAssignment: true,
+            values: {
+                'process.env.BABEL_TYPES_8_BREAKING': true,
+                'Buffer.isBuffer': 'Array.isArray',
+                'process.platform': '"unix"',
+                'process.env.BABEL_TYPES_8_BREAKING = true': '',
+                'process.env': '{}',
+                'export {load} from "./loader.mjs"': '',
+            },
+        }),
+    ],
+};
+
